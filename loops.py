f54f25ae1285d991c968b8c4656653995fb5d812
boris.alex.j <bo-a@yandex.ru>
2020-02-19 22:08:31 +0200

    Some polish to step 2

---
+++ loops.py
@@ -0,0 +1,13 @@
+for x in range(1,15):
+    print(x)
+
+print("------------")
+x = 0
+while x < 15:
+    print(x)
+    x = x + 1
+
+print("------------")
+array = ["a", "b", "c", "d", "e"]
+for letter in array:
+    print(letter)
