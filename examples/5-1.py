66189d29c3d353ca937682f87d107859103d429f
Stanislav Berko <stas.berko@ukr.net>
2020-02-19 17:21:35 +0200

    [add] array explanation for range

---
+++ examples/5-1.py
@@ -0,0 +1,8 @@
+from examples.endless_loop import draw_text, rendering, run
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+with run():
+    for num in numbers:
+        with rendering():
+            draw_text(num)
