97877d111bd8394ee2d5973638143f3d40c5b592
Stanislav Berko <stas.berko@ukr.net>
2020-02-19 17:07:59 +0200

    [add] condition explanation

---
+++ examples/3.py
@@ -0,0 +1,12 @@
+from examples.endless_loop import draw_text, rendering, run
+
+input_data = input("Input ->")
+
+with run():
+    with rendering():
+        if input_data == "q":
+            draw_text(input_data)
+        elif input_data == "w":
+            draw_text("You wrote W")
+        else:
+            draw_text("I don't know this letter")
