cd2ed55a646006b920e4dd1927d436c89267948d
Oleksii_Borys <Lopata_1>
2020-02-22 09:00:13 +0200

    switched second_step files

--- future steps/second_step.py
+++ future steps/second_step.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+import math
 
 # Intialize the pygame
 pygame.init()
@@ -7,29 +9,49 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
 # Background
-background = pygame.image.load('bg.jpg')
-
-
+background = pygame.image.load('assets/img/background/background1.png')
 
 # Caption and Icon
 pygame.display.set_caption("eKids project")
-icon = pygame.image.load('ufo.png')
+icon = pygame.image.load('assets/img/ufo.png')
 pygame.display.set_icon(icon)
 
 # Player
-playerImg = pygame.image.load('spaceship.1.png')
+playerImg = pygame.image.load('assets/img/player/player1.png')
 playerX = 370
 playerY = 480
 playerX_change = 0
+
+# Sound
+fireSound = pygame.mixer.Sound('assets/sound/sound.wav')
+
+# Step 2 - 1
+# Bullet
+# bulletImg = pygame.image.load('assets/img/bullet/bullet1.png')
+# bulletX = 0
+# bulletY = 480
+# bulletX_change = 0
+# bulletY_change = 10
+# bullet_state = "ready"
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
 
+# Step 2 - 3
+# def fire_bullet(x, y):
+#     global bullet_state # I don't like it. Can not use global to avoid unnecessary explanations ?
+#     bullet_state = "fire"
+#     screen.blit(bulletImg, (x + 16, y + 10))
+
+# Timer
+clock = pygame.time.Clock()
 
 # Game Loop
 running = True
 while running:
+
+    clock.tick(50)
 
     # RGB = Red, Green, Blue
     screen.fill((0, 0, 0))
@@ -46,17 +68,33 @@ while running:
             if event.key == pygame.K_RIGHT:
                 playerX_change = 5
 
+            if event.key == pygame.K_SPACE:
+                # Step 2 - 2
+                # if bullet_state is "ready":
+                #     # Get the current x cordinate of the spaceship
+                #     bulletX = playerX
+                #     fire_bullet(bulletX, bulletY)
+                    fireSound.play()
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
+    # Step 2 - 4
+    # Bullet Movement
+    # if bulletY <= 0:
+    #     bulletY = 480
+    #     bullet_state = "ready"
+    #
+    # if bullet_state is "fire":
+    #     fire_bullet(bulletX, bulletY)
+    #     bulletY -= bulletY_change
 
     playerX += playerX_change
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
-
 
     player(playerX, playerY)
     pygame.display.update()
