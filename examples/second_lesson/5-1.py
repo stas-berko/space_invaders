adac11bc3b7907094fabaab35ab51163107fb78c
boris.alex.j <bo-a@yandex.ru>
2020-02-24 21:58:10 +0200

    Init third step (as finished second)

--- examples/second_lesson/5-1.py
+++ examples/second_lesson/5-1.py
@@ -1,4 +1,4 @@
-from examples.endless_loop import draw_text, rendering, run
+from examples.second_lesson.endless_loop import draw_text, rendering, run
 
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
